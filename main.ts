be78931c9b55e47514eb1579d951d437496dcf25
imnyang <me@imnyang.xyz>
2024-11-28 11:13:54 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+let step = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(step)
+})
+input.onGesture(Gesture.Shake, function () {
+    step += 1
+})
 basic.forever(function () {
 	
 })
